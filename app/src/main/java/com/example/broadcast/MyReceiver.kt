cddf89f59f672b4567437e9c3efaf368ef56f8e4
OtsokovShA <shamil24@mail.ru>
2023-12-06 12:20:43 +0300

    Добавили собственный ресивер

--- app/src/main/java/com/example/broadcast/MyReceiver.kt
+++ app/src/main/java/com/example/broadcast/MyReceiver.kt
@@ -8,15 +8,27 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val action = intent?.action
-
-        when (action) {
-         //   Intent.ACTION_BATTERY_LOW -> {
-         //       Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
-         //   }
+        when (intent?.action) {
+            ACTION_CLICKED -> {
+                val count = intent.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText(context, "Clicked $count", Toast.LENGTH_SHORT).show()
+            }
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
-                Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show()
+                val turnedOn = intent.getBooleanExtra("state", false)
+                Toast.makeText(
+                    context,
+                    "Airplane mode changed. Turned on: $turnedOn",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
+    }
+    companion object {
+
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_COUNT = "count"
     }
 }
